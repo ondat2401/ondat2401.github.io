5c075ef565d3f3fd0a44d3d0e090710981fdcfcd
ondat2401 <ondat24@gmail.com>
2023-05-06 17:51:41 +0700

    up

--- js/js.js
+++ js/js.js
@@ -108,19 +108,70 @@ function login(username, password) {
     }
     return false; // Trả về false nếu thông tin đăng nhập sai
 }
-var STT=1;
+// var STT=1;
 
-function btngiohang(){
-    var hinh=getElementById('hinh').value;
-    var gia=getElementById('gia').value;
-    var size=getElementById('size').value;
-    var num=getElementById('num').value;
-    var tonggia=num*gia;
-    var trnew="<tr><td>"+STT+"</td><td>"+hinh+"</td><td>"+size+"</td><td>"+num+"</td><td>"+tonggia+"<\td><\tr>"
-    document.getElementById('tblgiohang')+=trnew;
-    STT++;
+// function btngiohang(){
+//     var hinh=getElementById('hinh').value;
+//     var gia=getElementById('gia').value;
+//     var size=getElementById('size').value;
+//     var num=getElementById('num').value;
+//     var tonggia=num*gia;
+//     var trnew="<tr><td>"+STT+"</td><td>"+hinh+"</td><td>"+size+"</td><td>"+num+"</td><td>"+tonggia+"</td></tr>"
+//     document.getElementById('tblgiohang')+=trnew;
+//     STT++;
+// }
+
+function checkquenmk(e) {
+    event.preventDefault();
+    // Lấy thông tin từ người dùng
+    var username = document.getElementById('usernameq').value;
+    var email = document.getElementById('emailq').value;
+    if(username==''||email==''){
+        alert("Vui lòng điền thông tin");
+        return false;
+    }
+    // Kiểm tra thông tin đăng nhập
+    var kiem=0;
+    for (var i = 0; i < users.length; i++) {
+        if (users[i].username === username && users[i].email === email) {
+            alert("Mật khẩu của bạn là:\n"+users[i].password);
+            kiem++;
+            return true; // Trả về true nếu thông tin đăng nhập đúng
+        }
+        if(kiem===0){
+            alert("Tài khoản hoặc email sai!");
+            return false;
+        }
+    }
 }
-function dangky(){
-    // window.location.href="dangnhap.html"
-    $('#Dangky').modal('show');
+function quenmk(username, email) {
+    // Lặp qua mảng người dùng để kiểm tra thông tin đăng nhập
+    for (var i = 0; i < users.length; i++) {
+        if (users[i].username === username && users[i].email === email) {
+            return true; // Trả về true nếu thông tin đăng nhập đúng
+        }
+    }
+    return false; // Trả về false nếu thông tin đăng nhập sai
 }
+function timkiem() {
+    // Lấy giá trị từ khóa tìm kiếm từ input
+    var searchTerm = document.getElementById("search-input").value.toLowerCase();
+    // Tìm các sản phẩm có từ khóa tìm kiếm
+    var products = document.querySelectorAll(".block");
+    for (var i = 0; i < products.length; i++) {
+      var product = products[i];
+      var keywords = product.getAttribute("data-keywords").toLowerCase();
+      // Hiển thị sản phẩm nếu chứa từ khóa tìm kiếm, ngược lại ẩn đi
+      if (keywords.indexOf(searchTerm) !== -1) {
+        product.style.display = "block";
+      } else {
+        product.style.display = "none";
+      }
+    }
+    // Ngăn chặn sự kiện mặc định của form
+    event.preventDefault();
+  }
+  function nutdk(){
+    window.location.href ="../html/dangnhap.html";
+
+  }

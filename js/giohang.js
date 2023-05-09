771f9cc5e56f956c689e0e7f4980533474a5a83e
ondat2401 <ondat24@gmail.com>
2023-05-09 11:35:21 +0700

    upload

--- js/giohang.js
+++ js/giohang.js
@@ -25,52 +25,56 @@ function themvaogiohang() {
     window.location.href = "../html/giohang.html"
   }
 
-function displayCartItems() {
-  const cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
-
-  const cartList = document.getElementById('cart-list');
-
-  cartList.innerHTML = '';
-
-  cartItems.forEach((item, index) => {
-    const listItem = document.createElement('li');
-
-    const itemHtml = `
-      <div class="cart-item">
-        <div class="cart-item-img">
-          
-        </div>
-        <div class="cart-item-info">
-        <table class="table table-bordered" style="width:80%;margin:auto;">
-        <tr>
-            <th>Hình ảnh</th>
-            <th>Tên sản phẩm</th>
-            <th>Kích thước</th>
-            <th>màu sắc</th>
-            <th>Số lượng</th>
-            <th>Tổng giá</th>
-            <th></th>
-        </tr>
-        <tr>
-        
-            <th><img src="${item.imgSrc}" alt="" width="200"></th>
-            <th>${item.productName}</th>
-            <th>${item.size}</th>
-            <th>${item.color}</th>
-            <th>${item.stock}</th>
-            <th>${item.quantity}</th>
-            <th><button class="btn" onclick="removeCartItem(${index})">Xóa</button></th>
-        </tr>
-        </table>
-          
-        </div>
-    `;
-
-    listItem.innerHTML = itemHtml;
-
-    cartList.appendChild(listItem);
-  });
-}
+  function displayCartItems() {
+    const cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+  
+    const cartList = document.getElementById('cart-list');
+  
+    cartList.innerHTML = '';
+  
+    let isHeaderAdded = false; // Biến để kiểm tra xem đã thêm dòng đầu chưa
+  
+    cartItems.forEach((item, index) => {
+      const listItem = document.createElement('li');
+  
+      const itemHtml = `
+        <div class="cart-item" >
+          <div class="cart-item-info">
+          <table class="table table-bordered" style="width:80%;margin:auto;">
+          ${!isHeaderAdded ? 
+            `<tr class="col-12">
+              <th class="col-sm-2">Hình ảnh</th>
+              <th class="col-sm-3">Tên sản phẩm</th>
+              <th class="col-sm-1">Kích thước</th>
+              <th class="col-sm-1">Màu sắc</th>
+              <th class="col-sm-1">Số lượng</th>
+              <th class="col-sm-2">Tổng giá</th>
+              <th class="col-sm-1"></th>
+            </tr>` : ''
+          }
+          <tr class="col-12">
+              <th class="col-sm-2"><img src="${item.imgSrc}" alt="" width="200"></th>
+              <th class="col-sm-3">${item.productName}</th>
+              <th class="col-sm-1">${item.size}</th>
+              <th class="col-sm-1">${item.color}</th>
+              <th class="col-sm-1">${item.stock} cái</th>
+              <th class="col-sm-2">${item.quantity}đ</th>
+              <th class="col-sm-1"><button class="btn btn-block" id="rmv-btn" style="line-height:200px;background-color: black;color: #d36905;" onclick="removeCartItem(${index})">Xóa</button></th>
+          </tr>
+          </table>
+          </div>
+      `;
+  
+      listItem.innerHTML = itemHtml;
+  
+      cartList.appendChild(listItem);
+  
+      if (!isHeaderAdded) {
+        isHeaderAdded = true; // Đặt biến này thành true để không thêm lại dòng đầu
+      }
+    });
+  }
+  
 function removeCartItem(index) {
     const cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
     cartItems.splice(index, 1);
